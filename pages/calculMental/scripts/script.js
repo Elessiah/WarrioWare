5ac693bf2cf7267e16973da54b4889a8c1d9dc0b
E221253S <matheo.souchet@etu.univ-nantes.fr>
2025-12-03 15:04:42 +0100

    refactor: remove "$" PHP syntax

--- pages/calculMental/scripts/script.js
+++ pages/calculMental/scripts/script.js
@@ -1,30 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     console.log("go !");
-    const $operation = ["+","-","*","/"];
-    let $calculText = document.getElementById("calcul");
-    let $answerText = document.getElementById("answer");
-    let $isValidAnswer = document.getElementById("isValidAnswer");
+    const operation = ["+","-","*","/"];
+    let calculText = document.getElementById("calcul");
+    let answerText = document.getElementById("answer");
+    let isValidAnswer = document.getElementById("isValidAnswer");
 
     //Génération des valeurs aléatoire
-    const $n1 = Math.floor(Math.random() * 20);
-    const $n2 = Math.floor(Math.random() * 20);
-    const $opeIndex = Math.floor(Math.random() * 5)
+    const n1 = Math.floor(Math.random() * 20);
+    const n2 = Math.floor(Math.random() * 20);
+    const opeIndex = Math.floor(Math.random() * 5)
 
     //Calcul du résultat
-    const $result = $n1 + $n2;
+    const result = n1 + n2;
 
     //Affichage du calcul
-    $calculText.innerText = `${$n1} ${$operation[$opeIndex]} ${$n2}`;
+    calculText.innerText = `${n1} ${operation[opeIndex]} ${n2}`;
 
     //Détection du clique et vérification du résultat
     document.addEventListener('keydown', function(event) {
         if (event.key === "Enter"){
-            if (Number($answerText.value) === $result ) {
-                $isValidAnswer.innerText = "Correct"
-                $isValidAnswer.className = "correct";
+            if (Number(answerText.value) === result ) {
+                isValidAnswer.innerText = "Correct"
+                isValidAnswer.className = "correct";
             } else {
-                $isValidAnswer.innerText = "Incorrect"
-                $isValidAnswer.className = "error";
+                isValidAnswer.innerText = "Incorrect"
+                isValidAnswer.className = "error";
             }
         }
 
